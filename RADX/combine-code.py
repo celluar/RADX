00488c1eaeba143e654dac8b1f23541fb012831f
Celly <boston@dhcp-10-103-255-32.wireless.northwestern.private>
2024-07-18 11:32:41 -0500

    addinng/orgainzing function and plotting

---
+++ RADX/combine-code.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.io import fits
+
+import functions
+import astropy.units as u
+import astropy.constants as const
+import matplotlib.pyplot as plt
+import numpy as np
